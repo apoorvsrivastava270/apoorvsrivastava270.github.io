a8e27f68169d403e9f2eab66eccadc0b2aafb02a
apoorvsrivastava270 <105517387+apoorvsrivastava270@users.noreply.github.com>
2022-05-30 13:02:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+} 
